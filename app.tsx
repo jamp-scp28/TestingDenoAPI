0dcd66dc5903e16c32f627c23c06b5c76765c317
jamp-scp28 <jorge.molano28@gmail.com>
2022-09-10 20:57:22 -0500

    deno test

--- app.tsx
+++ app.tsx
@@ -1,49 +1,136 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-// @deno-types="https://raw.githubusercontent.com/Soremwar/deno_types/4a50660/react/v16.13.1/react.d.ts"
-import React from "https://dev.jspm.io/react@16.13.1";
-import ReactDOMServer from "https://dev.jspm.io/react-dom@16.13.1/server";
-import { createApp } from "https://deno.land/x/servest/mod.ts";
+// @deno-types="https://denopkg.com/soremwar/deno_types/react/v16.13.1/react.d.ts"
+import React from "https://jspm.dev/react@17.0.2";
+// @deno-types="https://denopkg.com/soremwar/deno_types/react-dom/v16.13.1/server.d.ts"
+import ReactDOMServer from "https://jspm.dev/react-dom@17.0.2/server";
+// @deno-types="https://denopkg.com/soremwar/deno_types/react-dom/v16.13.1/react-dom.d.ts"
+import ReactDOM from "https://jspm.dev/react-dom@17.0.2";
+export { React, ReactDOM, ReactDOMServer };
+import { Application, Router } from "https://deno.land/x/oak@v6.0.1/mod.ts";
+import { writeJson, writeJsonSync } from 'https://deno.land/x/jsonfile/mod.ts';
+import { readJson, readJsonSync } from 'https://deno.land/x/jsonfile/mod.ts';
 
 const dbFile = await Deno.readFile("./db/db.json");
 
+interface Color{
+  id: number, 
+  color: string
+}
+
 class db {
-  constructor(file) {
+  file: any;
+  constructor(file: any) {
     this.file = file;  
   }
-
-  public saveColor(color: string){
-    await Deno.writeTextFile("./db/db.json", {color});
+  public async saveColor(newColor: string){
+    const colors: any = this.getColors();
+    const color: Color = {id: colors.length + 1, color: newColor}
+    colors.push(color)
+    writeJson("./db/db.json", colors);
   }
-
+  public getColors(): Array<Color>{
+    const colors: any = readJsonSync("./db/db.json");
+    return colors;
+  }
 }
 
 const dbDao = new db(dbFile);
+const app = new Application();
+const router = new Router();
 
-const app = createApp();
-app.handle("/", async (req) => {
-  await req.respond({
-    status: 200,
-    headers: new Headers({
-      "content-type": "text/html; charset=UTF-8",
-    }),
-    body: ReactDOMServer.renderToString(
-      <html>
-        <head>
-          <meta charSet="utf-8" />
-          <title>Deno API Testing</title>
-        </head>
-        <body>
-          <form>
-            <div class="form-group">
-              <label>Please Enter a Color:</label>
-              <input type="text" required="true" />
-            </div>
-            <button type="submit">Save Color</button>
-          </form>
-        </body>
-      </html>,
-    ),
-  });
+let colors: Array<Color> = []; 
+
+router.get("/", handlePage);
+
+router
+.get("/colors", async (context) => {
+  const colors = dbDao.getColors();
+  context.response.body = colors;
+})
+.post("/colors", async (context) => {
+  //Body should be: {name: "red"}
+  const body = context.request.body();
+  if (body.type === "json") {
+    const color: any = await body.value;
+    dbDao.saveColor(color.name);
+  }
+  context.response.body = { status: "OK" };
 });
 
-app.listen({ port: 8888 });
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+function App() {
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    const response = fetch("http://localhost:8000/colors", {
+      method: "POST",
+      body: event.data,
+    })
+  }
+  return (
+    <div>
+      <div className="jumbotron jumbotron-fluid bg-info">
+        <div className="container">
+          <h1 className="display-4">Colors</h1>
+          <p className="lead">Save your favorites colors here!</p>
+          <ListColors items={dbDao.getColors()} />
+        </div>
+      </div>
+      <div className="container">
+          <form onSubmit={handleSubmit}> 
+            <h3 className="display-8">Save a new color:</h3>
+            <div className="input-group">
+              <div className="input-group-prepend">
+                <span className="input-group-text">Color name:</span>
+              </div>
+              <input className="form-control" type="text" aria-label="color name" />
+            </div>
+            <button type="submit" className="btn btn-success mt-3">Save</button>
+          </form>
+      </div>
+    </div>
+  );
+}
+interface ListColors{
+  items: Color[]
+}
+function ListColors({ items = [] }: ListColors) {
+  return (
+    <div>
+      <ul className="list-group">
+        {items.map((item: any, index: number) => {
+          return (
+            <li key={index} className="list-group-item" style={{color:item.color}}>
+              {item.color}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+function handlePage(ctx: any) {
+  try {
+    const body = ReactDOMServer.renderToString(<App />);
+    ctx.response.body = `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+    #root{background-color: grey; width: 100%; height: 100vh}
+    </style>
+    <title>Document</title>
+  </head>
+  <body >
+    <div id="root">${body}</div>
+  </body>
+  </html>`;
+  }catch(error){
+    console.log(error);
+  }
+}
+
+console.log("server is running on http://localhost:8000/");
+await app.listen({ port: 8000 });
